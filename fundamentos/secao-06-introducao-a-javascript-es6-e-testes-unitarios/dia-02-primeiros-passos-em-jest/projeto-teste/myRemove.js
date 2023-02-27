0516ad534eb3667920ad3558c90482827367c60e
adrielle-b <adriielle@gmail.com>
2023-02-27 11:52:13 -0300

    exercicios de testes

---
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/projeto-teste/myRemove.js
@@ -0,0 +1,12 @@
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+
+  myRemove([1,2,3,4], 2);
+  module.exports = myRemove;
